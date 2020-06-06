82cba1d82dabe98bedb987554fdf9c7e7d43f531
gunawanzakiah <gunawanzakiah@gmail.com>
2020-06-06 19:51:09 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Support</title>
 </head>
+
 <body>
   <h1>Web Support</h1>
+  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit necessitatibus ipsa hic animi modi unde obcaecati, adipisci laborum nulla aut expedita nostrum doloremque at nemo veniam amet fuga, pariatur et?</p>
 </body>
+
 </html>
